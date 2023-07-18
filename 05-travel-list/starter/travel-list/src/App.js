0de6cf7d9d4f78a92bcb83a8eacfdcc511638105
Ayodeji Yekeen <ayodejiyou@gmail.com>
2023-07-18 10:13:32 +0000

    calculating statistics as derived state

--- 05-travel-list/starter/travel-list/src/App.js
+++ 05-travel-list/starter/travel-list/src/App.js
@@ -1,16 +1,34 @@
-const initialItems = [
-  { id: 1, description: "Passports", quantity: 2, packed: false },
-  { id: 2, description: "Socks", quantity: 12, packed: false },
-  { id: 3, description: "Charger", quantity: 1, packed: true },
-];
+import { useState } from "react";
 
 export default function App() {
+  const [items, setItems] = useState([]);
+
+  function handleItems(item) {
+    setItems((items) => [...items, item]);
+  }
+
+  function handleDeleteItem(id) {
+    setItems((items) => items.filter((item) => item.id !== id));
+  }
+
+  function handleToggleItem(id) {
+    setItems((items) =>
+      items.map((item) =>
+        item.id === id ? { ...item, packed: !item.packed } : item
+      )
+    );
+  }
+
   return (
     <div className="App">
       <Logo />
-      <Form />
-      <PackageList />
-      <Stats />
+      <Form onAddItems={handleItems} />
+      <PackageList
+        items={items}
+        onDeleteItem={handleDeleteItem}
+        onToggleItem={handleToggleItem}
+      />
+      <Stats items={items} />
     </div>
   );
 }
@@ -19,42 +37,102 @@ function Logo() {
   return <h1>🌴 Far Away 👜 </h1>;
 }
 
-function Form() {
+function Form({ onAddItems }) {
+  const [description, setDescription] = useState("");
+  const [quantity, setQuantity] = useState(1);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    if (!description) return;
+
+    const newItem = { description, quantity, packed: false, id: Date.now() };
+
+    onAddItems(newItem);
+
+    setDescription("");
+    setQuantity(1);
+  }
+
   return (
-    <div className="add-form">
+    <form className="add-form" onSubmit={(e) => handleSubmit(e)}>
       <h3>What do you need for your 😍 trip?</h3>
-    </div>
+      <select
+        value={quantity}
+        onChange={(e) => setQuantity(Number(e.target.value))}
+      >
+        {Array.from({ length: 20 }, (_, i) => i + 1).map((num) => (
+          <option value={num} key={num}>
+            {num}
+          </option>
+        ))}
+      </select>
+      <input
+        type="text"
+        placeholder="Item..."
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
+      <button>Add</button>
+    </form>
   );
 }
 
-function PackageList() {
+function PackageList({ items, onDeleteItem, onToggleItem }) {
   return (
     <div className="list">
       <ul>
-        {initialItems.map((item) => (
-          <Item item={item} key={item.id} />
+        {items.map((item) => (
+          <Item
+            item={item}
+            onDeleteItem={onDeleteItem}
+            onToggleItem={onToggleItem}
+            key={item.id}
+          />
         ))}
       </ul>
     </div>
   );
 }
 
-function Item({ item }) {
+function Item({ item, onDeleteItem, onToggleItem }) {
   return (
     <li>
+      <input
+        type="checkbox"
+        value={item.packed}
+        onChange={() => onToggleItem(item.id)}
+      />
+
       <span style={item.packed ? { textDecoration: "line-through" } : {}}>
         {item.quantity} {item.description}
       </span>
 
-      <button>❌</button>
+      <button onClick={() => onDeleteItem(item.id)}>❌</button>
     </li>
   );
 }
 
-function Stats() {
+function Stats({ items }) {
+  if (!items.lenth)
+    return (
+      <p className="footer">
+        <em>Start adding some items to your packing list</em>
+      </p>
+    );
+
+  const numItems = items.length;
+  const numItemsPacked = items.filter((item) => item.packed).length;
+  const percentage = Math.round((numItemsPacked / numItems) * 100);
+
   return (
     <footer className="stats">
-      <em>👜 You have X items on your list, and you already packed X (X%) </em>
+      <em>
+        {percentage === 100
+          ? "You got everything! Ready to go 🚀"
+          : `👜 You have ${numItems} items on your list, 
+          and you already packed ${numItemsPacked} (${percentage}%)`}
+      </em>
     </footer>
   );
 }
