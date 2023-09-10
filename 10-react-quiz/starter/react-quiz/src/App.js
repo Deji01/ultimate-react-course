4de7d23bb68f7c79dc4578800e4083fd808c8030
Ayodeji Yekeen <ayodejiyou@gmail.com>
2023-09-10 14:20:19 +0000

    managing related pieces of state - useReducer

--- 10-react-quiz/starter/react-quiz/src/App.js
+++ 10-react-quiz/starter/react-quiz/src/App.js
@@ -1,5 +1,11 @@
+import DateCounter from "./components/DateCounter";
+
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <DateCounter />
+    </div>
+  );
 }
 
 export default App;
