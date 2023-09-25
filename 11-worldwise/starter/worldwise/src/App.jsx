de81c7c4566dcfc9099f6c75c479c14bafa1d3e5
Ayodeji Yekeen <ayodejiyou@gmail.com>
2023-09-25 16:19:33 +0000

    Building the app layout

--- 11-worldwise/starter/worldwise/src/App.jsx
+++ 11-worldwise/starter/worldwise/src/App.jsx
@@ -1,4 +1,5 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import AppLayout from "./pages/AppLayout";
 import Homepage from "./pages/Homepage";
 import Login from "./pages/Login";
 import PageNotFound from "./pages/PageNotFound";
@@ -9,6 +10,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="app" element={<AppLayout />} />
         <Route path="/" element={<Homepage />} />
         <Route path="/login" element={<Login />} />
         <Route path="*" element={<PageNotFound />} />
